c7a30a2b3e8fa27b3a265c0c0bc6ac4311327a19
rodgers-munene <munenerodgers72@gmail.com>
2024-11-09 19:09:18 +0300

    category dropdown functionality

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -1,27 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './sidebar.css'
-import { CogIcon, UserIcon } from '@heroicons/react/outline'
+import { CogIcon, UserIcon, ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/outline'
+import { Categories } from '../../lib/data'
 
 
 
 const Sidebar = () => {
+  const[categoriesData, setCategoriesData] = useState([])
+  const[isDropDownVisible, setIsDropDownVisible] = useState(false)
+
+  const toggleDropDown = () =>{
+    setIsDropDownVisible(!isDropDownVisible)
+  }
+  
+
+  useEffect(() =>{
+    setCategoriesData(Categories)
+  }, [])
+
   return (
     <div className='sidebar-container'>
       {/* lists */}
       <div className='sidebar-list'>
         <ul className='sidebar-list_container'>
-          <li><a href='#'>Home</a></li>
-          <li><a href='#'>Categories</a></li>
-          <li><a href='#'>Sell</a></li>
-          <li><a href='#'>Contacts Us</a></li>
-          <li><a href='#'>Account</a></li>
+          <li className='main-list'>Home</li>
+
+          <li className={isDropDownVisible? 'categories-list': 'categories-list categories-hover'} onClick={toggleDropDown}>
+            <p className='categories-title'>Categories {isDropDownVisible? <ChevronUpIcon className='arrow-icons' width={20} height={20} /> : <ChevronDownIcon className='arrow-icons' width={20} height={20} />}</p>
+            {isDropDownVisible && (
+                <ul className='sub-categories'>
+                  {categoriesData? categoriesData.map((category, index) => (
+                    <li>{category.title}</li>
+                  )): ''} 
+                </ul>
+            )}
+           </li>
+
+
+          <li className='main-list'>Sell</li>
+          <li className='main-list'>Contacts Us</li>
+          <li className='main-list'>Account</li>
         </ul>
       </div>
 
       <div className='sidebar-profile'>
         <ul className='sidebar-profile_container'>
-          <li><a href='#'><CogIcon className='icon' width={20} height={20} />Settings</a></li>
-          <li><a href='#'><UserIcon className='icon' width={20} height={20} />Sign-in</a></li>
+          <li><CogIcon className='icon' width={20} height={20} />Settings</li>
+          <li><UserIcon className='icon' width={20} height={20} />Sign-in</li>
         </ul>
       </div>
 
