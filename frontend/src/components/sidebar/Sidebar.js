3de058174ab7a8b4d8b9fdd9c0079f89d57831ad
rodgers-munene <munenerodgers72@gmail.com>
2024-11-09 13:06:13 +0300

    sidebar

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import './sidebar.css'
+import { CogIcon, UserIcon } from '@heroicons/react/outline'
+
+
 
 const Sidebar = () => {
   return (
-    <div>
-        
+    <div className='sidebar-container'>
+      {/* lists */}
+      <div className='sidebar-list'>
+        <ul className='sidebar-list_container'>
+          <li><a href='#'>Home</a></li>
+          <li><a href='#'>Categories</a></li>
+          <li><a href='#'>Sell</a></li>
+          <li><a href='#'>Contacts Us</a></li>
+          <li><a href='#'>Account</a></li>
+        </ul>
+      </div>
+
+      <div className='sidebar-profile'>
+        <ul className='sidebar-profile_container'>
+          <li><a href='#'><CogIcon className='icon' width={20} height={20} />Settings</a></li>
+          <li><a href='#'><UserIcon className='icon' width={20} height={20} />Sign-in</a></li>
+        </ul>
+      </div>
+
     </div>
   )
 }
--- frontend/src/lib/data.js
+++ frontend/src/lib/data.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-async function FetchProducts() {
+export default async function FetchProducts() {
     try {
         const response = await axios.get('http://localhost:5000/products', {timeout: 5000})
         const data = response.data
@@ -11,4 +11,102 @@ async function FetchProducts() {
         console.error('Error fetching the products', error)
     }
 }
-export default FetchProducts
+
+export const Categories = [
+    {
+        title: 'Electronics',
+        subcategories: [
+            "Mobile Phones",
+            "Computer & Tablets",
+            "Cameras",
+            "Headphones & Earphones",
+            "Wearable Technology",
+            "Gaming Consoles & Accessories",
+            "Home Appliances"
+        ] 
+
+    },
+    {
+        title: "Fashion",
+        subcategories: [
+            "Men's Clothing",
+            "Women’s Clothing",
+            "Kids’ Clothing",
+            "Shoes",
+            "Accessories",
+            "Activewear & Sportswear"
+        ]
+  },
+  {
+        title: "Home & Kitchen",
+        subcategories: [
+            "Furniture",
+            "Home Decor",
+            "Kitchen Appliances",
+            "Bedding & Linens",
+            "Storage & Organization",
+            "Lighting"
+        ]
+  },
+  {
+        title: "Beauty & Health",
+        subcategories: [
+            "Skincare",
+            "Haircare",
+            "Makeup",
+            "Health Supplements",
+            "Personal Care",
+            "Fitness & Wellness"
+        ]
+  },
+  {
+        title: "Sports & Outdoor",
+        subcategories: [
+            "Exercise & Fitness Equipment",
+            "Outdoor Gear",
+            "Sports Apparel",
+            "Water Sports",
+            "Team Sports Gear"
+        ]
+  },
+  {
+        title: "Toys & Baby Products",
+        subcategories: [
+            "Toys & Games",
+            "Baby Clothing",
+            "Baby Essentials",
+            "Strollers & Car Seats",
+            "Educational Toys"
+        ]
+  },
+  {
+        title: "Automotive",
+        subcategories: [
+            "Car Accessories",
+            "Motorcycle Parts & Accessories",
+            "Tools & Equipment",
+            "Tires & Wheels"
+        ]
+  },
+  {
+        title: "Books & Media",
+        subcategories: [
+            "Books",
+            "Music",
+            "Movies",
+            "Video Games",
+            "Stationery"
+        ]
+  },
+  {
+        title: "Groceries & Essentials",
+        subcategories: [
+            "Food & Beverages",
+            "Cleaning Supplies",
+            "Pet Supplies",
+            "Household Essentials"
+        ]
+  }
+        
+    
+]
